5697db1861407451539f8c6442d9313aa7d77b1f
ibnualbienay <albieabinayy@gmail.com>
2026-08-19 20:00:51 +0700

    feat: add initial frontend library system

---
+++ detail-buku.js
@@ -0,0 +1,15 @@
+const params = new URLSearchParams(window.location.search);
+
+const bookId = params.get("book_id");
+
+const detailBuku = document.getElementById("detail-buku");
+const tombolBaca = document.getElementById("btn-baca-buku");
+const tombolFavorit = document.getElementById("btn-favorit");
+
+if (bookId) {
+    detailBuku.setAttribute("data-book-id", bookId);
+
+    tombolBaca.setAttribute("data-book-id", bookId);
+
+    tombolFavorit.setAttribute("data-book-id", bookId);
+}
